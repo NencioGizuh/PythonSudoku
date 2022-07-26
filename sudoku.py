bc4c74ebd54d427ea33b5077d2e689d05aa0e3fe
Nencio Paniagua <nencio@Nencios-MBP.paniagua.nl>
2022-07-26 19:52:36 +0200

    First draft of sudoku horizontal checking for double numbers

--- sudoku.py
+++ sudoku.py
@@ -43,27 +43,114 @@ def GenerateCellen(sudokuCounter):
     sudokuCounter += 1
     return cellen
 
+def CheckHorizontal(sudokuSubList):
+    cellenOptions = [1,2,3,4,5,6,7,8,9]
+    for index,item in enumerate(sudokuSubList):
+        if item == 0:
+            continue
+        elif item in cellenOptions:
+            cellenOptions.remove(item)
+        elif item not in cellenOptions:
+            newNumber = random.randint(0, len(cellenOptions))
+            sudokuSubList[index] = newNumber
+
+    #dicthorizontalOne[0], dicthoriztonal[1], dicthorizontal[2] -> 
+    
+    #|-----------| # |---------|
+    #|012|012|012|   |000111222|
+    #|345|345|345|   |000111222| --> sudokuHorizontalOne[0,1,2]
+    #|678|678|678|   |000111222|
+    #|---|---|---|   |---------|
+    #|012|012|012|   |333444555|
+    #|345|345|345|   |333444555| --> sudokuHorizontalTwo[0,1,2]
+    #|345|345|345|   |333444555|
+    #|---|---|---|   |---------|
+    #|012|012|012|   |666777888|
+    #|345|345|345|   |666777888| --> sudokuHorizontalThree[6,7,8]
+    #|678|678|678|   |666777888|
+    #|-----------|   |---------|
+
+
+    # We doen het per drie: 0,1,2 + 3,4,5 + 6,7,8
+    # We checken elke keer deze sequenties 0,1,2,0,1,2,0,1,2 + 3,4,5,3,4,5,3,4,5 + 6,7,8,6,7,8,6,7,8
+
+    #Dit wordt nu inefficient maar werk het eerst uit
+    #Maak een functie waarin je een subsudoku meegeeft en de starting index zoals
+def CreateHorizontalList(subHorizontal, horizontalListOne, horizontalListTwo, horizontalListThree):
+    for value in subHorizontal.values():
+        for sleutels, waardes in value.items():
+            try:
+                waarde = int(waardes)
+            except:
+                waarde = 0
+            if sleutels == 0 or sleutels == 1 or sleutels == 2:
+                horizontalListOne.append(waarde)
+            elif sleutels == 3 or sleutels == 4 or sleutels == 5:
+                horizontalListTwo.append(waarde)
+            elif sleutels == 6 or sleutels == 7 or sleutels == 8:
+                horizontalListThree.append(waarde)
+    return horizontalListOne, horizontalListTwo, horizontalListThree
+
 def GenerateSudoku():
     GenerateCellen(0)
     GenerateCellen(1)
     GenerateCellen(2)
+    GenerateCellen(3)
     GenerateCellen(4)
     GenerateCellen(5)
     GenerateCellen(6)
     GenerateCellen(7)
     GenerateCellen(8)
-    print(sudoku)
+
+    sudokuHorizontalListOne = []
+    sudokuHorizontalListTwo = []
+    sudokuHorizontalListThree = []
+    sudokuHorizontalListFour = []
+    sudokuHorizontalListFive = []
+    sudokuHorizontalListSix = []
+    sudokuHorizontalListSeven = []
+    sudokuHorizontalListEight = []
+    sudokuHorizontalListNine = []
+    dictHorizontalOne = {}
+    dictHorizontalTwo = {}
+    dictHorizontalThree = {}
+    dictHorizontalOne[0] = sudoku[0]
+    dictHorizontalOne[1] = sudoku[1]
+    dictHorizontalOne[2] = sudoku[2]
+    dictHorizontalTwo[0] = sudoku[3]
+    dictHorizontalTwo[1] = sudoku[4]
+    dictHorizontalTwo[2] = sudoku[5]
+    dictHorizontalThree[0] = sudoku[6]
+    dictHorizontalThree[1] = sudoku[7]
+    dictHorizontalThree[2] = sudoku[8]
+
+    CreateHorizontalList(dictHorizontalOne, sudokuHorizontalListOne, sudokuHorizontalListTwo, sudokuHorizontalListThree)
+    CreateHorizontalList(dictHorizontalTwo, sudokuHorizontalListFour, sudokuHorizontalListFive, sudokuHorizontalListSix)
+    CreateHorizontalList(dictHorizontalThree, sudokuHorizontalListSeven, sudokuHorizontalListEight, sudokuHorizontalListNine)    
+    CheckHorizontal(sudokuHorizontalListOne)
+    CheckHorizontal(sudokuHorizontalListTwo)
+    CheckHorizontal(sudokuHorizontalListThree)
+    CheckHorizontal(sudokuHorizontalListFour)
+    CheckHorizontal(sudokuHorizontalListFive)
+    CheckHorizontal(sudokuHorizontalListSix)
+    CheckHorizontal(sudokuHorizontalListSeven)
+    CheckHorizontal(sudokuHorizontalListEight)
+    CheckHorizontal(sudokuHorizontalListNine)
+    print(sudokuHorizontalListOne)
+    print(sudokuHorizontalListTwo)
+    print(sudokuHorizontalListThree)
+    print(sudokuHorizontalListFour)
+    print(sudokuHorizontalListFive)
+    print(sudokuHorizontalListSix)
+    print(sudokuHorizontalListSeven)
+    print(sudokuHorizontalListEight)
+    print(sudokuHorizontalListNine)
     return sudoku
 
 GenerateSudoku()
 
-
-def CheckHorizontal():
-    return
-
-def CheckVertical():
-    return
-
-def CheckNineCells():
+def CheckVertical(sudoku):
+# We doen het per drie 0,3,6 + 1,4,7 + 2,5,8
+# We checken elke keer deze sequenties 0,3,9,0,3,9,0,3,9 1,4,7,1,4,7,1,4,7 + 2,5,8,2,5,8,2,5,8
     return
 
